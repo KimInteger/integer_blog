73ed72570ec3bcceb204734149409fb22018dcf3
김정수 <kjs20151240@gmail.com>
2024-10-25 18:49:15 +0900

    feat | modal
    모달 기능 추가

--- frontend/src/context/AuthContext/authProvider.tsx
+++ frontend/src/context/AuthContext/authProvider.tsx
@@ -1,16 +1,39 @@
-import { useState } from "react";
+// src/context/AuthContext/authProvider.tsx
+
+import React, { useState } from "react";
 import { AuthContext } from "./authContext";
+import Modal from "../../ui/shared/modalComponent";
+import checkPassWord from "../../lib/checkPassword";
 
-const AuthProvider : React.FC<{children : React.ReactNode}> = ({ children }) => {
-  const [isLogined, setIsLogined] = useState(false)
+const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [isLogined, setIsLogined] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const login = () => {setIsLogined(true)}
-  const logout = () => {setIsLogined(false)}
-  return(
-    <AuthContext.Provider value={{isLogined, login, logout}}>
+  const login = () => {
+    setIsModalOpen(true); // 모달 열기
+  };
+
+  const logout = () => {
+    setIsLogined(false);
+  };
+
+  const handleLogin = (password: number) => {
+      // 비밀번호 체크 후 로그인 상태 변경
+      if (checkPassWord(password)) {
+        setIsLogined(true);
+        return true; // 로그인 성공
+      }
+      return false; // 로그인 실패
+    };
+
+  return (
+    <AuthContext.Provider value={{ isLogined, login, logout }}>
       {children}
+      {isModalOpen && (
+        <Modal onClose={() => setIsModalOpen(false)} onLogin={handleLogin} />
+      )}
     </AuthContext.Provider>
-  )
-}
+  );
+};
 
-export default AuthProvider
+export default AuthProvider;
