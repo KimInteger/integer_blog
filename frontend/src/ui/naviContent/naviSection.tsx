3b57f4ae0bfa491bbbea03b4545b41e6a4bbc133
김정수 <kjs20151240@gmail.com>
2024-10-25 18:48:26 +0900

    art | navigateSection.tsx
    스타일 수정 z-index값 수정

--- frontend/src/ui/naviContent/naviSection.tsx
+++ frontend/src/ui/naviContent/naviSection.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavigateSection = () => {
   return (
-    <div className="fixed top-[45%] left-0 w-[7vw] bg-white shadow-md z-50 p-4">
+    <div className="fixed top-[45%] left-0 w-[7vw] bg-white shadow-md z-5 p-4">
       <div className="flex flex-col gap-4">
         <a href="#home" className="hover:text-blue-500">home</a>
         <a href="#about" className="hover:text-blue-500">about</a>
