1e2fe867398a5325c7482c4dafe838f6b1a2837c
김정수 <kjs20151240@gmail.com>
2024-10-25 15:52:22 +0900

    style | app.tsx
    구역나누기

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import AuthProvider from "./context/AuthContext/authProvider";
 
 const App = ()=>{
   return (
-    <>
-      <div>
-        <h1>김정수의 블로그가 될 곳</h1>
-      </div>
-    </>
+    <AuthProvider>
+      <header>
+        <span>10vh</span>
+      </header>
+      <main>
+        <p>여가 80vh받으면 된다 아입니까.</p>
+        <div>고정 사이드 네비20vw</div>
+        <div>메인 컨텐츠80vw</div>
+      </main>
+      <footer><span>10vh</span></footer>
+    </AuthProvider>
   )
 }
 
