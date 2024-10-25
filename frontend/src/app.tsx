75f83f54d22d6b64479aea28745f20d4a40dad2d
김정수 <kjs20151240@gmail.com>
2024-10-25 17:48:26 +0900

    art | naviSection.tsx app.tsx
    스타일 조정

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -3,6 +3,7 @@ import AuthProvider from "./context/AuthContext/authProvider";
 import HeaderContent from "./ui/headerContent/headerSection";
 import { headerLiteral } from "./static/headerStatic/headerStatic";
 import NavigateSection from "./ui/naviContent/naviSection";
+import MainContent from "./ui/mainContent/mainContent";
 
 const App = ()=>{
   return (
@@ -11,10 +12,12 @@ const App = ()=>{
         <HeaderContent title={headerLiteral.title} />
       </header>
       <main className="flex flex-row w-full">
-        <div className="w-[5vw]">
+        <div className="w-[10vw]">
           <NavigateSection />
         </div>
-        <div className="w-[95vw]">메인 컨텐츠80vw</div>
+        <div className="w-[90vw]">
+          <MainContent />
+        </div>
       </main>
     </AuthProvider>
   )
--- frontend/src/ui/naviContent/naviSection.tsx
+++ frontend/src/ui/naviContent/naviSection.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 const NavigateSection = () => {
-  return(
-    <>
-      <div className="w-full flex flex-col gap-4">
-        <a href="#home">home</a>
-        <a href="#about">about</a>
-        <a href="#project">project</a>
+  return (
+    <div className="fixed top-[45%] left-0 w-[7vw] bg-white shadow-md z-50 p-4">
+      <div className="flex flex-col gap-4">
+        <a href="#home" className="hover:text-blue-500">home</a>
+        <a href="#about" className="hover:text-blue-500">about</a>
+        <a href="#project" className="hover:text-blue-500">project</a>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default NavigateSection
+export default NavigateSection;
