60194ad3fbc5badb4aa4c0ca6750c59a078e10e2
김정수 <kjs20151240@gmail.com>
2024-10-29 16:42:22 +0900

    style | app.tsx global.css scrollbar
    스크롤바 스타일링

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -12,23 +12,24 @@ const App = () => {
   return (
     <AuthProvider>
       <Router>
-        <header>
-          <HeaderContent title={headerLiteral.title} />
-        </header>
-        <main className="flex flex-row w-full">
-          <div className="w-[20vw]">
-            <NavigateSection />
-          </div>
-          <div className="w-[60vw]">
-            <Routes>
-              <Route index element={<MainHomeSection />} />
-              <Route path="/" element={<MainHomeSection />} />
-              <Route path="/about" element={<MainAboutSection />} />
-              <Route path="/project" element={<MainProjectSection />} />
-            </Routes>
-          </div>
-          <div className="w-[20vw]"></div>
-        </main>
+        <div className="bg-orange-200 h-screen overflow-hidden">
+          <header>
+            <HeaderContent title={headerLiteral.title} />
+          </header>
+          <main className="flex flex-row w-full h-[calc(100vh-7vh)] overflow-hidden">
+            <div className="w-[20vw]">
+              {/* <NavigateSection /> */}
+            </div>
+            <div className="w-[60vw] overflow-y-hidden">
+              <Routes>
+                <Route index element={<MainHomeSection />} />
+                <Route path="/about" element={<MainAboutSection />} />
+                <Route path="/project" element={<MainProjectSection />} />
+              </Routes>
+            </div>
+            <div className="w-[20vw]"></div>
+          </main>
+        </div>
       </Router>
     </AuthProvider>
   );
--- frontend/src/ui/mainContent/mainHome.tsx
+++ frontend/src/ui/mainContent/mainHome.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import routePush from "../../lib/routeFunc";
 
 const MainHomeSection = () => {
   return(
@@ -7,7 +8,7 @@ const MainHomeSection = () => {
       <h1>저는 개발자 김정수입니다.</h1>
       <h1>제 개인 패치노트에 오신것을 환영합니다.</h1>
       <div>
-      <button className="bg-orange-500 text-white font-semibold text-base px-4 py-2 rounded-md hover:bg-orange-600">
+      <button className="bg-orange-500 text-white font-semibold text-base px-4 py-2 rounded-md hover:bg-orange-600" onClick={routePush("/about")}>
         Knock Knock!
       </button>
       </div>
