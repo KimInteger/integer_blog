ce5d1ae1d204be5ce38a4c4a3305ab5f7dd691cc
김정수 <kjs20151240@gmail.com>
2024-10-29 16:12:18 +0900

    modi | naviSection
    a태그 없애고, navigate사용

--- frontend/src/ui/naviContent/naviSection.tsx
+++ frontend/src/ui/naviContent/naviSection.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import routePush from "../../lib/routeFunc";
 
 const NavigateSection = () => {
   return (
     <div className="fixed top-[45%] left-0 w-[7vw] bg-white shadow-md z-5 p-4">
       <div className="flex flex-col gap-4">
-        <a href="#home" className="hover:text-blue-500">home</a>
-        <a href="#about" className="hover:text-blue-500">about</a>
-        <a href="#project" className="hover:text-blue-500">project</a>
+        <button className="hover:text-orange-500" onClick={routePush('/')}>home</button>
+        <button className="hover:text-orange-500" onClick={routePush('/about')}>about</button>
+        <button className="hover:text-orange-500" onClick={routePush('/project')}>project</button>
       </div>
     </div>
   );
