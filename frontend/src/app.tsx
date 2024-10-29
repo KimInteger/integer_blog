d34e32a482f6b7ce20a3aba153a00fee8d8a2884
김정수 <kjs20151240@gmail.com>
2024-10-29 16:11:30 +0900

    modi | app.tsx
    route태그 사용

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,26 +1,36 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthProvider from "./context/AuthContext/authProvider";
 import HeaderContent from "./ui/headerContent/headerSection";
 import { headerLiteral } from "./static/headerStatic/headerStatic";
 import NavigateSection from "./ui/naviContent/naviSection";
-import MainContent from "./ui/mainContent/mainContent";
+import MainHomeSection from "./ui/mainContent/mainHome";
+import MainAboutSection from "./ui/mainContent/mainAbout";
+import MainProjectSection from "./ui/mainContent/mainProject";
 
-const App = ()=>{
+const App = () => {
   return (
     <AuthProvider>
-      <header>
-        <HeaderContent title={headerLiteral.title} />
-      </header>
-      <main className="flex flex-row w-full">
-        <div className="w-[10vw]">
-          <NavigateSection />
-        </div>
-        <div className="w-[90vw]">
-          <MainContent />
-        </div>
-      </main>
+      <Router>
+        <header>
+          <HeaderContent title={headerLiteral.title} />
+        </header>
+        <main className="flex flex-row w-full">
+          <div className="w-[20vw]">
+            <NavigateSection />
+          </div>
+          <div className="w-[60vw]">
+            <Routes>
+              <Route path="/" element={<MainHomeSection />} />
+              <Route path="/about" element={<MainAboutSection />} />
+              <Route path="/project" element={<MainProjectSection />} />
+            </Routes>
+          </div>
+          <div className="w-[20vw]"></div>
+        </main>
+      </Router>
     </AuthProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
