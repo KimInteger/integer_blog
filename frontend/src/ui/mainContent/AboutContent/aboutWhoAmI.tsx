1262d2b8b79d2113f81f9847a295aea0ac112719
김정수 <kjs20151240@gmail.com>
2024-10-30 19:48:20 +0900

    style | AboutContent
    report형태의 css형태 양식 할당

--- frontend/src/ui/mainContent/AboutContent/aboutWhoAmI.tsx
+++ frontend/src/ui/mainContent/AboutContent/aboutWhoAmI.tsx
@@ -1,10 +1,39 @@
-const AboutMe = () => {
-  return(
-    <>
-      <div>Hello Mr my yesterday</div>
-      <div>타임머신을 타고~</div>
-    </>
-  )
-}
+import React from 'react';
+import '../../../style/aboutReport.css'; // CSS 파일 임포트
 
-export default AboutMe
+const AboutMe = () => {
+  return (
+    <div className="report-container mt-4"> 
+      <h1 className="report-title">김정수는 무엇을 할 수 있는가!</h1>
+      <div className="report-divider" /> 
+      <div>
+        <p className="report-text">
+          이 주제는 두 가지로 나눌 수 있습니다. <br />
+          생산자 김정수와 개발자 김정수는 할 수 있는 것도 다르고, <br />
+          이룬 것도 다르기 때문입니다.
+        </p>
+      </div>
+      <div>
+        <h2 className="report-section-title">1. 생산자 김정수</h2>
+        <p className="report-text">
+          생산자 김정수는 콘텐츠를 기획하고 제작하는 데 주력하고 있습니다. 
+          다양한 매체를 통해 대중과 소통하며, 창의적인 아이디어를 바탕으로 
+          프로젝트를 진행합니다.
+        </p>
+      </div>
+      <div>
+        <h2 className="report-section-title">2. 개발자 김정수</h2>
+        <p className="report-text">
+          개발자 김정수는 웹 애플리케이션을 구축하고 유지보수하는 데 힘쓰고 있습니다. 
+          JavaScript와 최신 프레임워크를 사용하여 사용자 친화적인 경험을 제공하기 위해 
+          노력하고 있습니다.
+        </p>
+        <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae dolore, aut labore beatae quos tempora natus, odio deleniti, quam error similique ut! Architecto neque labore repudiandae quo quod corrupti sunt.Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae dolore, aut labore beatae quos tempora natus, odio deleniti, quam error similique ut! Architecto neque labore repudiandae quo quod corrupti sunt.Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae dolore, aut labore beatae quos tempora natus, odio deleniti, quam error similique ut! Architecto neque labore repudiandae quo quod corrupti sunt.Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae dolore, aut labore beatae quos tempora natus, odio deleniti, quam error similique ut! Architecto neque labore repudiandae quo quod corrupti sunt.Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae dolore, aut labore beatae quos tempora natus, odio deleniti, quam error similique ut! Architecto neque labore repudiandae quo quod corrupti sunt.Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae dolore, aut labore beatae quos tempora natus, odio deleniti, quam error similique ut! Architecto neque labore repudiandae quo quod corrupti sunt.Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae dolore, aut labore beatae quos tempora natus, odio deleniti, quam error similique ut! Architecto neque labore repudiandae quo quod corrupti sunt.Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae dolore, aut labore beatae quos tempora natus, odio deleniti, quam error similique ut! Architecto neque labore repudiandae quo quod corrupti sunt.Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae dolore, aut labore beatae quos tempora natus, odio deleniti, quam error similique ut! Architecto neque labore repudiandae quo quod corrupti sunt.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
--- frontend/src/ui/mainContent/AboutContent/justSadContent.tsx
+++ frontend/src/ui/mainContent/AboutContent/justSadContent.tsx
@@ -1,18 +1,24 @@
+import '../../../style/aboutReport.css'; // CSS 파일 임포트
+
 const SadSentence = () => {
-  return(
-    <div>
-      <p>너무 슬픈 일입니다.</p>
-      <br />
-      <p>그래도 찾아와 주셔서 감사합니다.</p>
-      <br />
-      <p>이왕 오신김에 제 깃허브와 이메일을 정보로 가져가주세요.</p>
-      <br />
-      <div className="text-3xl">
-        <p>깃허브 : https://github.com/KimInteger</p><br />
-        <p>이메일 : kjs20151240@gmail.com</p>
+  return (
+    <div className="report-container mt-[10vh]"> 
+        <p className="report-text">
+          너무 슬픈 일입니다.
+        </p><br />
+        <p className="report-text">
+        그래도 찾아와 주셔서 감사합니다.
+        </p><br />
+        <p className="report-text">
+        이왕 오신김에 제 깃허브와 이메일을 정보로 가져가주세요.
+        </p><br />
+        <div className="text-3xl">
+          <p>깃허브 : https://github.com/KimInteger</p>
+          <div className="report-divider" /> 
+          <p>이메일 : kjs20151240@gmail.com</p>
       </div>
     </div>
-  )
+  );
 }
 
 export default SadSentence
